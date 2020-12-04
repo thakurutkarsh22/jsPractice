77a6c4307e47cb1bf4b2f4b1e8fd74b63bbef6b0
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2020-12-05 02:08:59 +0530

    arrays polyfill methods

--- Data-Structure/Arrays/index.js
+++ Data-Structure/Arrays/index.js
@@ -1,60 +1,146 @@
-const items = [
-    {name: 'bike', price: 100},
-    {name: 'Tv', price: 200},
-    {name: 'Album', price: 10},
-    {name: 'Book', price: 5},
-    {name: 'Phone', price: 500},
-    {name: 'Computer', price: 1000},
-    {name: 'Keyboard', price: 25}
-]
+// const items = [
+//     {name: 'bike', price: 100},
+//     {name: 'Tv', price: 200},
+//     {name: 'Album', price: 10},
+//     {name: 'Book', price: 5},
+//     {name: 'Phone', price: 500},
+//     {name: 'Computer', price: 1000},
+//     {name: 'Keyboard', price: 25}
+// ]
+
+// // filter
+
+// const filteredItems = items.filter((items)=>{
+//     return items.price<=100
+// })
+
+// // map : convert an array and return an new Array
+
+// const mapItems = items.map((items)=>{
+//     return items.name;
+// })
+
+// // find : find a single object in an array;
+
+// const foundItem = items.find((item)=>{
+//     return items.price === 5;
+// });
+
+// // for each : just for iteration 
+
+// items.forEach((item)=>{
+//     console.log(item);
+// })
+
+// // some : so even if one is true it will return true for the whole array 
+
+// const hasInexpensiveitems = items.some((item)=>{
+//     return item.price <=100; 
+// })
+
+// // any : so every items has to fall under a condition if true then return true
+
+
+// const hasInexpensiveitems1 = items.every((item)=>{
+//     return item.price <=10000; 
+// })
+
+
+// // reduce 
+
+// const total = items.reduce((currentTotal, item)=>{
+//     return item.price + currentTotal;
+// },0)
+
+
+// // includes return if the val is inside the array or not. 
+
+// const arrNo = [1,2,3,45];
+// arrNo.includes(2); // true
+// arrNo.includes(7); // false 
+
+
+
+
+//// ---------------------- Pollyfill Functions --------------------------------
+
+// map 
+
+if (!Array.prototype.myMap) {
+    Array.prototype.myMap = function(callback) {
+       let newArray = [];
+        // iterate array elements
+        let context = this;
+       for(let item of this) {
+       // pass each element to callback and push response to new array
+         newArray.push(callback.call(context,item));
+       }
+       // return final array
+       return newArray;
+     }
+}
+// declare an array
+const arr = [10,20,30,40];
+// call custom map() on array to square each element or perform any     other operation
+const squaredArray = arr.myMap((i) => i * i);
+// prints [100,400,900,1600]
+console.log(squaredArray);
+
+
 
 // filter
 
-const filteredItems = items.filter((items)=>{
-    return items.price<=100
-})
+if (Array.prototype.myFilter === undefined) {
+    Array.prototype.myFilter = function(fn) {
+      var rv = [];
+      
+      for(var i=0, l=this.length; i<l; i++)
+        if (fn(this[i])) rv.push(this[i]);
+  
+      return rv;
+    };
+}
 
-// map : convert an array and return an new Array
+const filter = arr.myFilter((i) =>{
+   console.log(this);
+   return i>20;
 
-const mapItems = items.map((items)=>{
-    return items.name;
-})
-
-// find : find a single object in an array;
-
-const foundItem = items.find((item)=>{
-    return items.price === 5;
 });
-
-// for each : just for iteration 
-
-items.forEach((item)=>{
-    console.log(item);
-})
-
-// some : so even if one is true it will return true for the whole array 
-
-const hasInexpensiveitems = items.some((item)=>{
-    return item.price <=100; 
-})
-
-// any : so every items has to fall under a condition if true then return true
+console.log(filter);
 
 
-const hasInexpensiveitems1 = items.every((item)=>{
-    return item.price <=10000; 
-})
+// find // findIndex 
+
+Array.prototype.myFind = function(callback, thisArg) {
+    if(!callback || typeof callback !== 'function') throw TypeError();
+    const size = this.length;
+    const that = thisArg || this;
+    for(var i = 0; i < size; i++) {
+        try {
+            if(!!callback.apply(that,[this[i], i, this])) {
+                return this[i];
+            }
+        } catch(e) {
+            return undefined;
+        }
+    }
+    return undefined;
+}
 
 
-// reduce 
 
-const total = items.reduce((currentTotal, item)=>{
-    return item.price + currentTotal;
-},0)
+//reduce
 
-
-// includes return if the val is inside the array or not. 
-
-const arrNo = [1,2,3,45];
-arrNo.includes(2); // true
-arrNo.includes(7); // false 
+Array.prototype.reduce = function(callbackFn, initialValue) {
+    if(!callbackFn || typeof callbackFn !== 'function') throw TypeError();
+    var len = this.length;
+    var i = 0;
+    if(typeof initialValue === 'undefined' || initialValue === null) {
+      initialValue =  this[0];
+      ++i;
+    } 
+    for(; i < len; i++) {
+      initialValue = callbackFn.apply(this, [initialValue, this[i], i, this])
+    }
+    return initialValue;
+  }
--- Var/index.js
+++ Var/index.js
@@ -31,3 +31,58 @@ console.log(x,y);
 console.log("t: " + t);
 // console.log("k: " + k);
 console.log("p: " + p);
+
+
+
+
+
+
+// Add a new method to the Shape class's prototype, calcPerimeter(), which calculates its perimeter (the length of the shape's outer edge) and logs the result to the console.
+// Create a new instance of the Shape class called square. Give it a name of square and a sideLength of 5.
+// Call your calcPerimeter() method on the instance, to see whether it logs the calculation result to the browser DevTools' console as expected.
+// Create a new instance of Shape called triangle, with a name of triangle and a sideLength of 3.
+// Call triangle.calcPerimeter() to check that it works OK.
+
+
+
+
+
+function Shape(name, sides, sideLength) {
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+  }
+
+
+Shape.prototype.calcPerimeter = function () {
+    console.log(this.sideLength * this.sides);   
+}
+
+const square = new Shape('square', 4 ,5);
+square.calcPerimeter();
+
+
+class ShapeEs6 {
+
+    constructor(name,sides,sideLength) {
+        this.name = name;
+        this.sides = sides;
+        this.sideLength = sideLength;
+    }
+
+    calcPerimeter() {
+        console.log(this.sideLength * this.sides);
+    }
+
+    get a() {
+
+    }
+
+    set a(value) {
+
+    }
+}
+
+let obj = new ShapeEs6('a',3,2);
+obj.a = 123;
+obj.a
