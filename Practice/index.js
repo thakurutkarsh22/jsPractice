41bbf68be73b8bf06900390b6f03853c54447514
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2021-08-02 03:45:03 +0530

    practice dump

--- Practice/index.js
+++ Practice/index.js
@@ -1,232 +1,337 @@
-// let sum = a => b => b ? sum(a+b) : a;
+// // let sum = a => b => b ? sum(a+b) : a;
 
-// let problem = sum(1)(2)(3)(14)();
-// console.log(problem);
+// // let problem = sum(1)(2)(3)(14)();
+// // console.log(problem);
 
 
-// // recursion
+// // // recursion
 
-// let user ={
-//     name: "Akshay Saini",
-//     address: {
-//       personal: {
-//         city: "Dehradun",
-//         state: "Uttrakhand",
-//         area: "Majra",
-//       },
-//       office: {
-//         city: "Hyderabad",
-//         area: {
-//           landmark: "Hi Tech",
-//         }
-//       }
-//     }
-// }
+// // let user ={
+// //     name: "Akshay Saini",
+// //     address: {
+// //       personal: {
+// //         city: "Dehradun",
+// //         state: "Uttrakhand",
+// //         area: "Majra",
+// //       },
+// //       office: {
+// //         city: "Hyderabad",
+// //         area: {
+// //           landmark: "Hi Tech",
+// //         }
+// //       }
+// //     }
+// // }
 
-// let magic = (obj, parent, finalObj) => {
-//     for(let key in obj){
-//         console.log(key);
-//         if(typeof obj[key] === "object"){
-//             magic(obj[key], parent + "_" + key, finalObj);
-//         }
-//         else {
-//             finalObj[parent + "_" + key] = obj[key];
-//         }
-//     }
-// }
+// // let magic = (obj, parent, finalObj) => {
+// //     for(let key in obj){
+// //         console.log(key);
+// //         if(typeof obj[key] === "object"){
+// //             magic(obj[key], parent + "_" + key, finalObj);
+// //         }
+// //         else {
+// //             finalObj[parent + "_" + key] = obj[key];
+// //         }
+// //     }
+// // }
 
-// let finalObj= {};
-// magic(user, "user", finalObj);
+// // let finalObj= {};
+// // magic(user, "user", finalObj);
 
-// console.log(finalObj);
+// // console.log(finalObj);
   
-// const clickFunction = function() {
-//   console.log('clicked');
-//   // console.log(this);
-//   this.classList.toggle("abc");
+// // const clickFunction = function() {
+// //   console.log('clicked');
+// //   // console.log(this);
+// //   this.classList.toggle("abc");
+// // }
+
+// // const div = document.getElementById('abc');
+
+// // div.setAttribute('name' , 'red');
+
+
+// // div.addEventListener('click', clickFunction.bind(div) );
+// // // console.log(div.);
+
+
+
+
+// /// -- advance stuff----
+
+
+// // const obj1 = {
+// //   property1: a12
+// // }
+
+// // const descripton = Object.getOwnPropertyDescriptor(on);
+// // Object.hasOwnProperty();
+
+// // let arr = ["abcd", "defg"];
+
+
+// // abc();
+// // abc();
+
+// // function abc() {
+// //   let x = 0;
+// //   console.log(++x);
+// // }
+
+
+// // function attachEventListner() {
+// //   let count = 0;
+
+// //   document.getElementById("button").addEventListener("click", function click() {
+// //     console.log(++count);
+// //   })
+// // }
+
+// // attachEventListner();
+
+
+// // closure 
+
+// // function abc(y) {
+
+// //   const x = y;
+// //   const p = y;
+
+// //   const inside = () => {
+// //     console.log(x, p, window.innerHeight);
+// //   }
+
+// //   inside();
+// // }
+
+// // abc(2);
+// // abc(3);
+
+// // const a = () => {
+
+// // }
+
+
+
+
+
+// // function parseData(data) { /* Some computation */ return data;  };
+
+// // class User {
+// // 	constructor(name, ...data) {
+// // 		const parsedData = parseData(data);
+
+// // 		this.name = name;
+// // 		this.data = parsedData;
+
+// // 		// return parsedData;
+// // 	}
+
+// // 	getData() {
+// // 		return this.data;
+// // 	}
+// // }
+
+// // const Yomesh = new User('Yomesh', { youtubeChannel: 'https://bit.ly/devtools-yt', shouldSubscribe: true });
+
+// // // What would be the output of the following statement?
+// // console.log(Yomesh.data); 
+
+
+// // let newArr = input.fl
+
+
+
+// // tricky this question ofr arguments 
+
+// var length = 10;
+// function fn() {
+//   console.log(this.length);
 // }
-
-// const div = document.getElementById('abc');
-
-// div.setAttribute('name' , 'red');
-
-
-// div.addEventListener('click', clickFunction.bind(div) );
-// // console.log(div.);
-
-
-
-
-/// -- advance stuff----
-
-
-// const obj1 = {
-//   property1: a12
-// }
-
-// const descripton = Object.getOwnPropertyDescriptor(on);
-// Object.hasOwnProperty();
-
-// let arr = ["abcd", "defg"];
-
-
-// abc();
-// abc();
-
-// function abc() {
-//   let x = 0;
-//   console.log(++x);
-// }
-
-
-// function attachEventListner() {
-//   let count = 0;
-
-//   document.getElementById("button").addEventListener("click", function click() {
-//     console.log(++count);
-//   })
-// }
-
-// attachEventListner();
-
-
-// closure 
-
-// function abc(y) {
-
-//   const x = y;
-//   const p = y;
-
-//   const inside = () => {
-//     console.log(x, p, window.innerHeight);
+// var obj = {  
+//   length: 5,  
+//   method: function(fn) {    
+//     fn();    
+//     let f = arguments[0];
+//     // f();
 //   }
+// };
+// obj.method(fn, 1)
 
-//   inside();
+
+// function a(){
+//   setTimeout(() => {
+//     console.log("set time out")
+//   }, 0)
+//   Promise.resolve("solved").then(res => console.log(res));
 // }
 
-// abc(2);
-// abc(3);
 
-// const a = () => {
+// ​function abc(){
+//   for(var i=0;i<5;i++){
+//     setTimeout(function(){console.log(i)}, 500)
+//   }   
+// }   
+// abc()
 
+
+// var name = 'ruhi'   
+// var a = {
+//   name: "rohan",
+//   printName: () => {console.log(this.name)},
+//   pName: function() {
+//     console.log(this.name);
+//   }
+// }   
+// a.printName();
+// a.pName();
+
+
+
+
+// function deepCopy (obj) {
+//   const res = {}
+//   const keys = Object.keys(obj)
+//   keys.forEach(key => {
+//     if (typeof obj[key] === 'object') {
+//       res[key] = deepCopy(obj[key])
+//     } else {
+//       res[key] = obj[key]
+//     }
+//   })
+//   return res;
 // }
 
 
-
-
-
-// function parseData(data) { /* Some computation */ return data;  };
-
-// class User {
-// 	constructor(name, ...data) {
-// 		const parsedData = parseData(data);
-
-// 		this.name = name;
-// 		this.data = parsedData;
-
-// 		// return parsedData;
-// 	}
-
-// 	getData() {
-// 		return this.data;
-// 	}
+// var abc = {
+//   name: 'john',
+//   age: 20,
+//   address: {
+//     city: 'bangalore',
+//     state: 'tamilnadu',
+//     street: {
+//       pincode: 11111,
+//       houseno: 234
+//     }
+//   }   
 // }
 
-// const Yomesh = new User('Yomesh', { youtubeChannel: 'https://bit.ly/devtools-yt', shouldSubscribe: true });
 
-// // What would be the output of the following statement?
-// console.log(Yomesh.data); 
-
-
-// let newArr = input.fl
+// // importynt to know the type of obj 
+// if(Object.prototype.toString.call(arrayList) === '[object Array]') {
+//   console.log('Array!');
+// }
 
 
+// // js associativity rule 
+// var z = 1, y = z = typeof y;
+// console.log(y);
 
-// tricky this question ofr arguments 
+// // assignments return the rightmost value
+// let a;
+// console.log(a = 'dignity!') // dignity!
 
-var length = 10;
-function fn() {
-  console.log(this.length);
+
+// setTimeout(() => {
+//   console.log(abcd);
+//   var abcd  = 12;
+// }, 1000);
+
+
+// // console.log(abc)
+// // var abc = 89;
+// // console.log(abcd)
+
+// setTimeout(() => {
+//   console.log("as");
+//   console.log(abcd);
+// }, 2000);
+
+
+
+//  ar 
+
+// let a = true; 
+// let ak = "12";
+// setTimeout(() => {
+//   a = false;
+// }, 2000);
+
+// console.log(a)
+
+
+// closures
+
+// let a = 19;
+// function ac(){
+//   console.log(a);
+// }
+// ac();
+
+
+// different types of function 
+let fun1 = function helloFunction() {
+  console.log("hello abc")
 }
-var obj = {  
-  length: 5,  
-  method: function(fn) {    
-    fn();    
-    let f = arguments[0];
-    // f();
-  }
-};
-obj.method(fn, 1)
 
-
-function a(){
-  setTimeout(() => {
-    console.log("set time out")
-  }, 0)
-  Promise.resolve("solved").then(res => console.log(res));
+let fun2 = function () {
+    console.log("hello anynomoys ")
 }
 
 
-​function abc(){
-  for(var i=0;i<5;i++){
-    setTimeout(function(){console.log(i)}, 500)
-  }   
-}   
-abc()
+
+ async function abc() {
+   return await 10;
+}
+
+let variable1 = abc(); // 1 2 3 [] // promise<any>
+
+variable1.t
 
 
-var name = 'ruhi'   
-var a = {
-  name: "rohan",
-  printName: () => {console.log(this.name)},
-  pName: function() {
-    console.log(this.name);
-  }
-}   
-a.printName();
-a.pName();
+// backeend code 
 
+let promise1 = new Promise((res,rej)=> {
+  setInterval(()=> {
+    res([1,2,3,4]); // 5 ser
+  }, 5000)
+  
+})
 
+// front 
 
-
-function deepCopy (obj) {
-  const res = {}
-  const keys = Object.keys(obj)
-  keys.forEach(key => {
-    if (typeof obj[key] === 'object') {
-      res[key] = deepCopy(obj[key])
-    } else {
-      res[key] = obj[key]
-    }
+function getData(params) {
+  promise1.then(data => {
+    console.log(data);    
   })
-  return res;
+
+  // task a
+  console.log("a");
+
+  // tas b 
+
 }
 
+// getData();
 
-var abc = {
-  name: 'john',
-  age: 20,
-  address: {
-    city: 'bangalore',
-    state: 'tamilnadu',
-    street: {
-      pincode: 11111,
-      houseno: 234
-    }
-  }   
+
+
+async function fetch(params) {
+  let result  = await promise1;
+
+  // task a 
+  console.log("a")
+  console.log(result);
 }
 
+fetch()
 
-// importynt to know the type of obj 
-if(Object.prototype.toString.call(arrayList) === '[object Array]') {
-  console.log('Array!');
+let arr = ["utkarsh", "ayushi", "rohan", "romit"]
+
+function sortFunction(params) {
+  //logic
 }
 
+arr.sort();
 
-// js associativity rule 
-var z = 1, y = z = typeof y;
-console.log(y);
 
-// assignments return the rightmost value
-let a;
-console.log(a = 'dignity!') // dignity!
