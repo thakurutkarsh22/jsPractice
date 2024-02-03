296fbc3b0822b48f098013cc5451449f54eeb580
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-02-03 16:10:17 +0530

    abort controller

---
+++ LLD/1.Abort Controller/script.js
@@ -0,0 +1,22 @@
+// cancel the ongoing API request if it is not fulfilled.
+
+const controller = new AbortController();
+const signal = controller.signal;
+const url = "video.mp4";
+
+const downloadBtn = document.querySelector("#download");
+const abortBtn = document.querySelector("#abort");
+
+downloadBtn.addEventListener("click", async () => {
+  try {
+    const response = await fetch(url, { signal });
+    console.log("Download complete", response);
+  } catch (error) {
+    console.error(`Download error: ${error.message}`);
+  }
+});
+
+abortBtn.addEventListener("click", () => {
+  controller.abort();
+  console.log("Download aborted");
+});
